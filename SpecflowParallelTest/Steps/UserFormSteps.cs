60866d2165ce3192a4c54fd48734ed61c374cb27
Sunny Khar <skhar@autino.com>
2019-10-04 17:06:41 +0530

    Keep Origin

--- SpecflowParallelTest/Steps/UserFormSteps.cs
+++ SpecflowParallelTest/Steps/UserFormSteps.cs
@@ -1,98 +1,98 @@
-﻿//using OpenQA.Selenium;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
-//using OpenQA.Selenium.Remote;
-//using TechTalk.SpecFlow;
-//using TechTalk.SpecFlow.Assist;
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Remote;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
 
-//namespace SpecflowParallelTest.Steps
-//{
-//    [Binding]
-//    class UserFormSteps
-//    {
-//        private RemoteWebDriver _driver;
+namespace SpecflowParallelTest.Steps
+{
+    [Binding]
+    class UserFormSteps
+    {
+        private RemoteWebDriver _driver;
 
-//        public UserFormSteps(RemoteWebDriver driver)
-//        {
-//            _driver = driver;
-//        }
-
-
-
-//        [Given(@"I start entering user form details like")]
-//        public void GivenIStartEnteringUserFormDetailsLike(Table table)
-//        {
-//            dynamic data = table.CreateDynamicInstance();
-//            _driver.FindElement(By.Id("Initial")).SendKeys((string)data.Initial);
-//            _driver.FindElement(By.Id("FirstName")).SendKeys((string)data.FirstName);
-//            _driver.FindElement(By.Id("MiddleName")).SendKeys((string)data.MiddleName);
-//            _driver.FindElement(By.Name("english")).Click();
-//            Thread.Sleep(2000);
-//        }
-
-//        [Given(@"I click submit button")]
-//        public void GivenIClickSubmitButton()
-//        {
-//            _driver.FindElement(By.Name("Save")).Click();
-//        }
-
-//        [Given(@"I verify the entered user form details in the application database")]
-//        [Then(@"I verify the entered user form details in the application database")]
+        public UserFormSteps(RemoteWebDriver driver)
+        {
+            _driver = driver;
+        }
 
 
 
-//        public void GivenIVerifyTheEnteredUserFormDetailsInTheApplicationDatabase(Table table)
-//        {
-//            //Mock data collection
-//            List<AUTDatabase> mockAUTData = new List<AUTDatabase>()
-//            {
-//                new AUTDatabase()
-//                {
-//                    FirstName = "Karthik",
-//                    Initial = "k",
-//                    MiddleName = "k"
-//                },
+        [Given(@"I start entering user form details like")]
+        public void GivenIStartEnteringUserFormDetailsLike(Table table)
+        {
+            dynamic data = table.CreateDynamicInstance();
+            _driver.FindElement(By.Id("Initial")).SendKeys((string)data.Initial);
+            _driver.FindElement(By.Id("FirstName")).SendKeys((string)data.FirstName);
+            _driver.FindElement(By.Id("MiddleName")).SendKeys((string)data.MiddleName);
+            _driver.FindElement(By.Name("english")).Click();
+            Thread.Sleep(2000);
+        }
 
-//                new AUTDatabase()
-//                {
-//                    FirstName = "Prashanth",
-//                    Initial = "k",
-//                    MiddleName = "k"
-//                }
-//            };
+        [Given(@"I click submit button")]
+        public void GivenIClickSubmitButton()
+        {
+            _driver.FindElement(By.Name("Save")).Click();
+        }
 
-//            //For verification with single row data
-//            var result = table.FindAllInSet(mockAUTData);
-
-//            //For verification againt Multiple row data
-//            var resultnew = table.FindAllInSet(mockAUTData);
-
-//        }
-
-//        [Then(@"I logout of application")]
-//        public void ThenILogoutOfApplication()
-//        {
-//          //  ScenarioContext.Current.Pending();
-//        }
-
-
-//    }
-
-//    public class AUTDatabase
-//    {
-//        public string Initial { get; set; }
-
-//        public string FirstName { get; set; }
-
-//        public string MiddleName { get; set; }
-
-//        public string Gender { get; set; }
-//    }
+        [Given(@"I verify the entered user form details in the application database")]
+        [Then(@"I verify the entered user form details in the application database")]
 
 
 
-//}
+        public void GivenIVerifyTheEnteredUserFormDetailsInTheApplicationDatabase(Table table)
+        {
+            //Mock data collection
+            List<AUTDatabase> mockAUTData = new List<AUTDatabase>()
+            {
+                new AUTDatabase()
+                {
+                    FirstName = "Karthik",
+                    Initial = "k",
+                    MiddleName = "k"
+                },
+
+                new AUTDatabase()
+                {
+                    FirstName = "Prashanth",
+                    Initial = "k",
+                    MiddleName = "k"
+                }
+            };
+
+            //For verification with single row data
+            var result = table.FindAllInSet(mockAUTData);
+
+            //For verification againt Multiple row data
+            var resultnew = table.FindAllInSet(mockAUTData);
+
+        }
+
+        [Then(@"I logout of application")]
+        public void ThenILogoutOfApplication()
+        {
+          //  ScenarioContext.Current.Pending();
+        }
+
+
+    }
+
+    public class AUTDatabase
+    {
+        public string Initial { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string Gender { get; set; }
+    }
+
+
+
+}
