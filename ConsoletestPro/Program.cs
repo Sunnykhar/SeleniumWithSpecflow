6d6f1faf9b5804759adf85bc021b6cdbdc6cee8d
Sunny Khar <skhar@autino.com>
2022-01-25 18:45:41 +0530

    Add console app

---
+++ ConsoletestPro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoletestPro
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void unused()
+        {
+
+        }
+
+        public void unused1()
+        {
+            int a;
+        }
+    }
+}
